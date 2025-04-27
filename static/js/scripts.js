be7c7c300f78624ba02f3f4f80e975b32e7cdfea
Thiago Cosme dos Santos <tiagomutle@hotmail.com>
2025-04-27 03:54:38 -0300

    update index

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,17 @@
+
+// console.log(selectedRadio) // Valor do input selecionado
+
+
 console.log("bem vindo")
+
+document.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    
+    const selectedRadio = document.querySelector("input[name='perfil']:checked").value
+    localStorage.setItem("perfil", selectedRadio);
+    
+    let perfil = localStorage.getItem("perfil");
+    console.log(perfil)
+
+})
+
