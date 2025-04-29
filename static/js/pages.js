9f70952f209d5b2f8bf7849872f241929140b80c
ThiagoSantos1986 <tiagomutle@hotmail.com>
2025-04-29 17:51:05 -0300

    create cadastro

--- static/js/pages.js
+++ static/js/pages.js
@@ -4,6 +4,9 @@
 const reset =  document.getElementById('reset')
 const backgroud = document.getElementById('back')
 const logout = document.getElementById('logout')
+const linkMenu = document.getElementsByClassName('color-user')
+
+
 
 function perfil(){
     
@@ -16,16 +19,19 @@ function perfil(){
         case 'Castanheira':
             console.log('pagina Castanheira')
             backgroud.style.background = 'var(--background-Castanheira)'
+            document.body.style.color = 'var(--color-castanha)'
             break
 
         case 'Peroba-Rosa':
             console.log('pagina peroba')
             backgroud.style.background = 'var(--background-Peroba-rosa)'
+            document.body.style.color = 'var(--color-peroba-rosa)'
             break
             
         default:
             console.log('pagina Pau-Brasil')
             backgroud.style.background = 'var(--background-PauBrasil)'
+            document.body.style.color = 'var(--color-pau-brasil)'
             
     }
 
