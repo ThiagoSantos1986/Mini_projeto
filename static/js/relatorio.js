d84f0045a10054a1c1e93df2c0873c13aceb6da1
ThiagoSantos1986 <tiagomutle@hotmail.com>
2025-05-01 17:53:54 -0300

    finalizado Relatorio js e html

--- static/js/relatorio.js
+++ static/js/relatorio.js
@@ -6,51 +6,67 @@ perfil();
 const inputOption = document.getElementById('especie')
 const user = document.getElementById('user')
 const search = document.getElementById('search')
+const table = document.getElementById("table");
 
 
 search.addEventListener('click', ( ) =>{
-    console.log(user.value)
-    console.log(inputOption.value)
+    
+   
 
-    const result = listObjeto.filter((nome) => nome.user ===  user.value.toUpperCase() ||  nome.especie === inputOption.value)
+    let result = []
+    
+    if(!(user.value || inputOption.value)){
+        
+        alert('Necessario Um dos campos Preenchido')
+        
+        return
+    }
+    if((user.value || inputOption.value)){
+
+        result = listObjeto.filter((nome) => nome.user ===  user.value.toUpperCase() || nome.especie === inputOption.value)
+    }
+    
+    if (inputOption.value && user.value){
+        
+        result = listObjeto.filter((nome) => nome.user ===  user.value.toUpperCase() && nome.especie === inputOption.value)
+        
+    }
     
     if(result.length == 0){
-        console.log("aqui")
+        alert('NENHUM REGISTRO LOCALIZADO')
+        inputOption.value = ''
+        user.value = ''
+        return
     }
+    
+    createTable(result)
 
-    console.log(result)
+    inputOption.value = ''
+    user.value = ''
+    
 })
 
 
 
 
-function createTable() {
-        
-    const table = document.getElementById("table");
-    // const row = table.insertRow(1);
-    // const colUser = row.insertCell(0);
-    // const colEspecie = row.insertCell(1);
-    // const colQuantidade = row.insertCell(2);
-    
-    const result = listObjeto.filter((nome) => nome.user ===  'ANA' ||  nome.especie === 'Ipês')
+function createTable(result) {
+   //aqui no while foi usado IA pra ajudar 
+    while (table.rows.length > 1) {
+        table.deleteRow(1);
+    }
 
-    
+    table.style.display = 'table';
+
     for (const element of result) {
         const row = table.insertRow(1);
         const colUser = row.insertCell(0);
         const colEspecie = row.insertCell(1);
         const colQuantidade = row.insertCell(2);
-    
-        colUser.innerHTML = element.user
-        colEspecie.innerHTML = element.especie
-        colQuantidade.innerHTML = element.quantidade
+        colUser.innerHTML = element.user;
+        colEspecie.innerHTML = element.especie;
+        colQuantidade.innerHTML = element.quantidade;
     }
-
-
 }
-
-
-
 
 
 
